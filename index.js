bbb7d9eac19474fe19c922ed9e2b381c60aaa518
NgoVanLoc <62205ngovanloc@gmail.com>
2025-03-27 21:01:02 +0700

    Lan dau cua em

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv  from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.status(200).json({
+        message: "Welcome",
+        name: "Node.js App",
+        version: "1.0.0"
+    })
+});
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`); 
+});
+
+
+
+
+// Khác nhau giữa require và import
+// So sánh giữa WEB server (Ngin, Apache) và Web BE
+
+// nodemon: phát hiện thay đổi trong code
+// tìm hiểu những gì vừa code và các folder ó trong project 
